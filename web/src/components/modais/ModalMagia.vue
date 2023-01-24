d8ec5c0c2e9991bb48fa6460cfd4754a3e768867
Your NameKaue <dkauealves2@gmail.com>
2023-01-24 02:20:07 -0300

    Adição e Remoção de Magias

--- web/src/components/modais/ModalMagia.vue
+++ web/src/components/modais/ModalMagia.vue
@@ -1,32 +1,17 @@
 <template>
   <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#modalMagias"
-  >
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalMagias">
     Lista de Magias
   </button>
 
   <!-- Modal -->
-  <div
-    class="modal fade modal-fullscreen"
-    id="modalMagias"
-    tabindex="-1"
-    aria-labelledby="modalMagiasLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade modal-fullscreen" id="modalMagias" tabindex="-1" aria-labelledby="modalMagiasLabel"
+    aria-hidden="true">
     <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title white" id="modalMagiasLabel">Modal title</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <h5 class="modal-title white" id="modalMagiasLabel">Lista de Magias</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <div class="row table-responsive">
@@ -40,6 +25,7 @@
                   <th>Duration</th>
                   <th>Components</th>
                   <th>Description</th>
+                  <th>Adicionar/Remover</th>
                 </thead>
                 <tbody>
                   <tr class="" v-for="magia in magias" :key="magia.name">
@@ -51,9 +37,17 @@
                     <td class="">{{ magia.components.raw }}</td>
                     <td class="">{{ magia.description }}</td>
                     <td>
-                      <button @click.prevent="adicionarMagia" class="btn">
-                        +
-                      </button>
+                      <div class="row">
+                        <div class="col-6"><button v-if="magias_aprendidas.includes(magia)"
+                            class="btn bg-danger w-100 mb-4" @click.prevent="removerMagia(magia)">
+                            -
+                          </button></div>
+
+                        <div class="col-6"><button @click.prevent="adicionarMagia(magia)" class="btn w-100 mb-2">
+                            +
+                          </button>
+                        </div>
+                      </div>
                     </td>
                   </tr>
                 </tbody>
@@ -62,12 +56,7 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button
-            @click.prevet="console.log('eba')"
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
             Fechar
           </button>
         </div>
@@ -79,14 +68,18 @@
 <script lang="ts">
 import jsonSpells from "@/assets/spells.json";
 export default {
+  props: ["magias_aprendidas"],
   data() {
     return {
       magias: jsonSpells,
     };
   },
   methods: {
-    adicionarMagia() {
-      console.log("tresd");
+    adicionarMagia(magia: any) {
+      this.$emit("adicionarMagia", magia);
+    },
+    removerMagia(magia: any) {
+      this.$emit("removerMagia", magia);
     },
   },
 };
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -10,23 +10,23 @@
               <div class="col-9">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Nome do Personagem</label>
-                  <input type="text" class="form-control" id="nome" v-model="this.personagem.nome"
-                    aria-describedby="nomeHelp">
+                  <input type="text" class="form-control" id="nome" v-model="personagem.nome"
+                    aria-describedby="nomeHelp" />
                 </div>
               </div>
-              <!-- NOME -->
+              <!-- NIVEL -->
               <div class="col-3">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Nível</label>
                   <input type="number" maxlength="2" min="3" max="99" class="form-control" id="nivel"
-                    v-model="this.personagem.nivel" aria-describedby="nivelHelp">
+                    v-model="personagem.nivel" aria-describedby="nivelHelp" />
                 </div>
               </div>
               <!-- CLASSE -->
               <div class="col-6">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Classe</label>
-                  <select name="classe" v-model="this.personagem.classe" class="form-select" aria-label="">
+                  <select name="classe" v-model="personagem.classe" class="form-select" aria-label="">
                     <option selected>Escolha sua classe</option>
                     <option value="Barbarian">Barbarian</option>
                     <option value="Bard">Bard</option>
@@ -49,15 +49,15 @@
               <div class="col-6">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Sub-Classe</label>
-                  <input type="text" class="form-control" id="sub_classe" v-model="this.personagem.sub_classe"
-                    aria-describedby="sub_classeHelp">
+                  <input type="text" class="form-control" id="sub_classe" v-model="personagem.sub_classe"
+                    aria-describedby="sub_classeHelp" />
                 </div>
               </div>
               <!-- RAÇA -->
               <div class="col-6">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Raça</label>
-                  <select name="raca" v-model="this.personagem.raça" class="form-select" aria-label="">
+                  <select name="raca" v-model="personagem.raça" class="form-select" aria-label="">
                     <option selected>Escolha sua raça</option>
                     <option value="Dragonborn">Dragonborn</option>
                     <option value="Dwarf">Dwarf</option>
@@ -76,90 +76,94 @@
               <div class="col-6">
                 <div class="mb-3">
                   <label for="nome" class="form-label">Sub-Raça</label>
-                  <input type="text" class="form-control" id="sub_raca" v-model="this.personagem.sub_raca"
-                    aria-describedby="sub_racaHelp">
+                  <input type="text" class="form-control" id="sub_raca" v-model="personagem.sub_raca"
+                    aria-describedby="sub_racaHelp" />
                 </div>
               </div>
               <!-- Atributos -->
-              <div v-for="(atributo, key) in this.personagem.atributos[0]" class="col-2">
+              <div v-for="(atributo, key) in personagem.atributos[0]" class="col-2 mb-4">
                 <div class="mb-3">
                   <label for="nome" class="form-label">{{ key }}</label>
                   <input type="number" maxlength="2" min="3" max="99" class="form-control" :value="atributo.valor"
-                    id="nome" aria-describedby="nomeHelp">
+                    id="nome" aria-describedby="nomeHelp" />
                 </div>
               </div>
-              <!-- Deslocamento -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="deslocamento" class="form-label">Deslocamento</label>
-                  <input type="text" v-model="this.personagem.deslocamento" class="form-control" id="iniciativa"
-                    aria-describedby="deslocamentoHelp">
-                </div>
-              </div>
-              <!-- CLASSE DE ARMADURA -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="ca" class="form-label">CA</label>
-                  <input type="text" v-model="this.personagem.ca" class="form-control" id="ca"
-                    aria-describedby="caHelp">
-                </div>
-              </div>
-              <!-- PROFICIENCIA -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="proficiencia" class="form-label">Proficiência</label>
-                  <input type="text" v-model="this.personagem.proficiencia" class="form-control" id="ca"
-                    aria-describedby="proficienciaHelp">
-                </div>
-              </div>
-              <!-- PONTOS DE VIDA ATUAL -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="hp_atual" class="form-label">HP Atual</label>
-                  <input type="number" v-model="this.personagem.hp.atual" class="form-control" id="hp_atual"
-                    aria-describedby="hp_atualHelp">
-                </div>
-              </div>
-              <!-- PONTOS DE VIDA MAXIMO-->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="hp_max" class="form-label">HP MAX</label>
-                  <input type="number" v-model="this.personagem.hp.max" class="form-control" id="hp_max"
-                    aria-describedby="hp_maxHelp">
-                </div>
-              </div>
-              <!-- PONTOS DE VIDA TEMPORARIO -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="hp_temp" class="form-label">HP Temporário</label>
-                  <input type="number" v-model="this.personagem.hp.temp" class="form-control" id="hp_temp"
-                    aria-describedby="hp_tempHelp">
-                </div>
-              </div>
-              <!-- DADOS DE VIDA -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="hp_temp" class="form-label">Hit Dice | {{ this.personagem.hit_dice.valor }}</label>
-                  <input type="number" v-model="this.personagem.hit_dice.quant" class="form-control" id="hp_temp"
-                    aria-describedby="hp_tempHelp">
-                </div>
-              </div>
-              <!-- INICIATIVA -->
-              <div class="col-3">
-                <div class="mb-3">
-                  <label for="iniciativa" class="form-label">Iniciativa</label>
-                  <input type="text" v-model="this.personagem.iniciativa" class="form-control" id="iniciativa"
-                    aria-describedby="iniciativaHelp">
+              <div class="col-12 mb-4">
+                <div class="row">
+                  <!-- Deslocamento -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="deslocamento" class="form-label">Deslocamento</label>
+                      <input type="text" v-model="personagem.deslocamento" class="form-control" id="iniciativa"
+                        aria-describedby="deslocamentoHelp" />
+                    </div>
+                  </div>
+                  <!-- CLASSE DE ARMADURA -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="ca" class="form-label">CA</label>
+                      <input type="text" v-model="personagem.ca" class="form-control" id="ca"
+                        aria-describedby="caHelp" />
+                    </div>
+                  </div>
+                  <!-- PROFICIENCIA -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="proficiencia" class="form-label">Proficiência</label>
+                      <input type="text" v-model="personagem.proficiencia" class="form-control" id="ca"
+                        aria-describedby="proficienciaHelp" />
+                    </div>
+                  </div>
+                  <!-- PONTOS DE VIDA ATUAL -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="hp_atual" class="form-label">HP Atual</label>
+                      <input type="number" v-model="personagem.hp.atual" class="form-control" id="hp_atual"
+                        aria-describedby="hp_atualHelp" />
+                    </div>
+                  </div>
+                  <!-- PONTOS DE VIDA MAXIMO-->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="hp_max" class="form-label">HP MAX</label>
+                      <input type="number" v-model="personagem.hp.max" class="form-control" id="hp_max"
+                        aria-describedby="hp_maxHelp" />
+                    </div>
+                  </div>
+                  <!-- PONTOS DE VIDA TEMPORARIO -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="hp_temp" class="form-label">HP Temporário</label>
+                      <input type="number" v-model="personagem.hp.temp" class="form-control" id="hp_temp"
+                        aria-describedby="hp_tempHelp" />
+                    </div>
+                  </div>
+                  <!-- DADOS DE VIDA -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="hp_temp" class="form-label">Hit Dice | {{ personagem.hit_dice.valor }}</label>
+                      <input type="number" v-model="personagem.hit_dice.quant" class="form-control" id="hp_temp"
+                        aria-describedby="hp_tempHelp" />
+                    </div>
+                  </div>
+                  <!-- INICIATIVA -->
+                  <div class="col-3">
+                    <div class="mb-3">
+                      <label for="iniciativa" class="form-label">Iniciativa</label>
+                      <input type="text" v-model="personagem.iniciativa" class="form-control" id="iniciativa"
+                        aria-describedby="iniciativaHelp" />
+                    </div>
+                  </div>
                 </div>
               </div>
               <!-- DEATH SAVES -->
               <div class="col-12 mb-4">
                 <div class="row">
                   <p>DEATH SAVES</p>
-                  <div v-for="ds in this.deathSaves" class="form-check col">
+                  <div v-for="ds in deathSaves" class="form-check col" v-bind:key="ds">
                     <div class="form-check">
                       <input class="form-check-input" name="deathSaves[]" type="checkbox"
-                        v-model="this.personagem.deathSaves" :value="ds" :id="'flexCheckDefault' + ds">
+                        v-model="personagem.deathSaves" :value="ds" :id="'flexCheckDefault' + ds" />
                       <label class="form-check-label" for="flexCheckDefault">
                         {{ ds }}
                       </label>
@@ -167,42 +171,48 @@
                   </div>
                 </div>
               </div>
+
               <!-- pericias -->
-              <div class="col-12 mb-4">
-                <div class="row">
-                  <p>PERÍCIAS</p>
-                  <div v-for="skill in this.pericias" class="form-check col-6">
+
+
+                <p>PERÍCIAS</p>
+                <div class="row mb-4">
+                  <div v-for="skill in pericias" class="form-check col-6">
                     <div class="form-check">
-                      <input class="form-check-input" name="pericias[]" type="checkbox"
-                        v-model="this.personagem.pericias" :value="skill" id="flexCheckDefault">
+                      <input class="form-check-input" name="pericias[]" type="checkbox" v-model="personagem.pericias"
+                        :value="skill" id="flexCheckDefault" />
                       <label class="form-check-label" for="flexCheckDefault">
                         {{ skill.nome }}
                       </label>
                     </div>
                   </div>
                 </div>
-              </div>
+    
+
               <div class="col-12 mb-4">
                 <div class="row">
-                  <textarea name="magias" id="magias" rows="10" disabled>{{ this.personagem.magia.aprendidas }}</textarea>
-                  <modalMagias/>
+                  <div v-if="personagem.magia.aprendidas[0]" class="card mb-4 pt-4 pb-4 bg-default mb-4 col-12">
+                    <p class="mb-0">MAGIAS</p>
+                    <ul class="mt-2 mb-2 row">
+                      <li class="col-md-6" v-for="(ma, index) in personagem.magia.aprendidas" v-bind:key="index">
+                        {{ ma.name }}
+                      </li>
+                    </ul>
+                  </div>
+
+                  <modalMagias @adicionarMagia="adicionarMagias" @removerMagia="removerMagias"
+                    :magias_aprendidas="personagem.magia.aprendidas" />
                 </div>
-                
               </div>
-            <div class="col-12 d-flex text-right">  <!-- Magias -->
-                <button type="submit" class="ms-auto btn ">Cadastrar</button></div>
+              <div class="col-12 d-flex text-right">
+                <!-- Magias -->
+              </div>
             </div>
           </form>
-      
         </div>
       </div>
-      <div class="card text-start bg-default mb-4">
-        <div class="card-body">
-          {{ this.personagem }}
-        </div>
-      </div>
-    </div>
 
+    </div>
   </div>
 </template>
 
@@ -213,16 +223,14 @@
 <script lang="ts">
 import jsonSkills from "@/assets/skills.json";
 import modalMagias from "@/components/modais/ModalMagia.vue";
+import { throwStatement } from "@babel/types";
 export default {
-  components:{
-    modalMagias
+  components: {
+    modalMagias,
   },
   data() {
     return {
-      deathSaves: ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'],
-      magia_selecionada: "",
-      magia_aprendida_selecionada: "",
-      magia_memorizada_selecionada: "",
+      deathSaves: ["STR", "DEX", "CON", "INT", "WIS", "CHA"],
       pericias: jsonSkills,
       personagem: {
         nome: "",
@@ -238,17 +246,17 @@ export default {
         },
         hit_dice: {
           quant: 1,
-          valor: "D" + 10
+          valor: "D" + 10,
         },
         atributos: [
           {
-            "STR": { valor: 10, mod: 0 },
-            "DEX": { valor: 10, mod: 0 },
-            "CON": { valor: 10, mod: 0 },
-            "INT": { valor: 10, mod: 0 },
-            "WIS": { valor: 10, mod: 0 },
-            "CHA": { valor: 10, mod: 0 },
-          }
+            STR: { valor: 10, mod: 0 },
+            DEX: { valor: 10, mod: 0 },
+            CON: { valor: 10, mod: 0 },
+            INT: { valor: 10, mod: 0 },
+            WIS: { valor: 10, mod: 0 },
+            CHA: { valor: 10, mod: 0 },
+          },
         ],
         deathSaves: [],
         proficiencia: 2,
@@ -270,12 +278,24 @@ export default {
             "8": 1,
             "9": 1,
           },
-        }
-      }
-    }
+        },
+      },
+    };
   },
   methods: {
-
+    adicionarMagias(magia: Object) {
+      if (!this.personagem.magia.aprendidas.includes(magia)) {
+        this.personagem.magia.aprendidas.push(magia);
+      }
+    },
+    removerMagias(magia: Object) {
+      this.arrayRemove(this.personagem.magia.aprendidas, magia);
+    },
+    arrayRemove(arr: Array<Object>, value: Object) {
+      this.personagem.magia.aprendidas = arr.filter(function (ele) {
+        return ele != value;
+      });
+    },
   },
-}
+};
 </script>
